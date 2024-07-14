b49968fe10bdf99ce5f7b3c4c8615a5c2935f6e9
RuslanMarchuk <rusmar@qamadness.com>
2024-07-14 13:09:35 +0200

    Test Form Laptop

--- Hillel Course/Java Automation Course/src/main/java/org/example/Main.java
+++ Hillel Course/Java Automation Course/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import testFromLaptop.TestFromLaptop;
 import testFromPc.TestFromPc;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -10,6 +11,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         TestFromPc.test();
+        TestFromLaptop.test();
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
---
+++ Hillel Course/Java Automation Course/src/main/java/testFromLaptop/TestFromLaptop.java
@@ -0,0 +1,7 @@
+package testFromLaptop;
+
+public class TestFromLaptop {
+    public static void test(){
+        System.out.println("Test From Laptop");
+    }
+}
