be3f5b580b98157fd8bc0fd5baa6cd0e4428ccf6
Ruslan Marchuk <mckruslan@gmail.com>
2024-07-17 21:33:11 +0200

    first commit for classwork 05

--- Java Automation Course/src/main/java/testFromLaptop/TestFromLaptop.java
+++
@@ -1,7 +0,0 @@
-package main.java.testFromLaptop;
-
-public class TestFromLaptop {
-    public static void test(){
-        System.out.println("Test From Laptop");
-    }
-}
--- Java Automation Course/src/main/java/testFromPc/TestFromPc.java
+++
@@ -1,7 +0,0 @@
-package main.java.testFromPc;
-
-public class TestFromPc {
-    public static void test(){
-        System.out.println("Test from PC");
-    }
-}
